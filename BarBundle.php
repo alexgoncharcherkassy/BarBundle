b41c9372ecd75505c25e3b624c9d06c7d7ebc8ac
Aleksandr Gonchar <shuric2005@mail.ru>
2021-04-11 14:28:39 +0300

    First commit

---
+++ BarBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AlexGoncharCK\BarBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BarBundle extends Bundle
+{
+}
---
+++ DependencyInjection/BarExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AlexGoncharCK\BarBundle\DependencyInjection;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class BarExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yml');
+    }
+}
